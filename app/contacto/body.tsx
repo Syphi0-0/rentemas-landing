5b4c7beee5e9d10c1df2a1573d8d67a0df5958d3
Syphi0-0 <syphi.dev@gmail.com>
2024-03-30 09:04:04 -0700

    new body

--- app/contacto/body.tsx
+++ app/contacto/body.tsx
@@ -1,12 +1,11 @@
-import style from './hoverone.module.css';
+import style from "./hoverone.module.css";
 
 type Props = {
   children?: React.ReactNode;
 };
 
 const Body = ({ children }: Props) => {
-  return <div className={style.body}>{children}
-  </div>;
+  return <div className={style.bdy}>{children}</div>;
 };
 
 export default Body;
