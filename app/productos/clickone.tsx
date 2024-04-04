ed05bc07a01df04b5f58e459d2e43e065b20a3c3
Syphi0-0 <syphi.dev@gmail.com>
2024-04-03 18:44:55 -0700

    nomames no hice nada

--- app/productos/clickone.tsx
+++ app/productos/clickone.tsx
@@ -21,78 +21,80 @@ function Hoverone() {
         </button>
       </div>
       {selected && (
-        <div className={style.card}>
-          <div className={style.bdytxt}>
-            <h2 className={style.title}>
-              EQUIPO DE CONSTRUCCIÓN Y MAQUINARIA LIGERA EN RENTA Y VENTA.
-            </h2>
-            <ul>
-              <li>
-                Maquinaria ligera de las mejores marcas como CIPSA MQ, JOPER,
-                ENAR, MAKITA, ETC.
-              </li>
-              <li>
-                Bailarinas jaguar, vibradores de gasolina y eléctricos,
-                revolvedoras, rompedores, cinceladoras, plantas de luz,
-                soldadoras.
-              </li>
-              <li>
-                Motores pequeños de las mejores marcas como Kohler, Honda,
-                Robin, Subaru, etc.
-              </li>
-              <li>Refacciones para esta maquinaria y motores.</li>
-              <li>Herramienta para concreto marca MARSHALLTOWN.</li>
-              <li>Discos diamantados para corte de concreto.</li>
-            </ul>
-            <button onClick={toggle} className={style.vrtxt}>
-              {selected ? "Ver menos" : "Ver más"}
-            </button>
+        <label>
+          <div className={style.card}>
+            <div className={style.bdytxt}>
+              <h2 className={style.title}>
+                EQUIPO DE CONSTRUCCIÓN Y MAQUINARIA LIGERA EN RENTA Y VENTA.
+              </h2>
+              <ul>
+                <li>
+                  Maquinaria ligera de las mejores marcas como CIPSA MQ, JOPER,
+                  ENAR, MAKITA, ETC.
+                </li>
+                <li>
+                  Bailarinas jaguar, vibradores de gasolina y eléctricos,
+                  revolvedoras, rompedores, cinceladoras, plantas de luz,
+                  soldadoras.
+                </li>
+                <li>
+                  Motores pequeños de las mejores marcas como Kohler, Honda,
+                  Robin, Subaru, etc.
+                </li>
+                <li>Refacciones para esta maquinaria y motores.</li>
+                <li>Herramienta para concreto marca MARSHALLTOWN.</li>
+                <li>Discos diamantados para corte de concreto.</li>
+              </ul>
+              <button onClick={toggle} className={style.vrtxt}>
+                {selected ? "Ver menos" : "Ver más"}
+              </button>
+            </div>
+            <div className={style.bdyimg}>
+              <div className={style.inimg}>
+                <Image
+                  width={300}
+                  height={220}
+                  src={"/10752.jpg"}
+                  alt={"lacaca2"}
+                />
+                <Image
+                  width={300}
+                  height={220}
+                  src={"/14857.jpg"}
+                  alt={"lacaca2"}
+                />
+              </div>
+              <div className={style.inimg}>
+                <Image
+                  width={300}
+                  height={220}
+                  src={"/13739.jpg"}
+                  alt={"lacaca3"}
+                />
+                <Image
+                  width={300}
+                  height={220}
+                  src={"/13786.jpg"}
+                  alt={"lacaca3"}
+                />
+              </div>
+              <div className={style.inimg}>
+                <Image
+                  width={300}
+                  height={220}
+                  src={"/11571.jpg"}
+                  alt={"lacaca4"}
+                />
+                <Image
+                  width={300}
+                  height={220}
+                  src={"/14807.jpg"}
+                  alt={"lacaca4"}
+                />
+              </div>
+            </div>
           </div>
-          <div className={style.bdyimg}>
-            <div className={style.inimg}>
-              <Image
-                width={300}
-                height={220}
-                src={"/10752.jpg"}
-                alt={"lacaca2"}
-              />
-              <Image
-                width={300}
-                height={220}
-                src={"/14857.jpg"}
-                alt={"lacaca2"}
-              />
-            </div>
-            <div className={style.inimg}>
-              <Image
-                width={300}
-                height={220}
-                src={"/13739.jpg"}
-                alt={"lacaca3"}
-              />
-              <Image
-                width={300}
-                height={220}
-                src={"/13786.jpg"}
-                alt={"lacaca3"}
-              />
-            </div>
-            <div className={style.inimg}>
-              <Image
-                width={300}
-                height={220}
-                src={"/11571.jpg"}
-                alt={"lacaca4"}
-              />
-              <Image
-                width={300}
-                height={220}
-                src={"/14807.jpg"}
-                alt={"lacaca4"}
-              />
-            </div>
-          </div>
-        </div>
+        </label>
       )}
     </div>
   );
